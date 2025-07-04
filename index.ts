622894e846fead621856d149063c3f04911e3d68
Piers Karsenbarg <piers@pulumi.com>
2025-07-04 13:59:36 +0100

    fixed program

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as std from "@pulumi/std";
 
-export const b64 = std.base64decode({
+export const b64 = std.base64encode({
     input: "1234"
 });
